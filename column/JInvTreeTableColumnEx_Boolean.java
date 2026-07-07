00f6001627a187d47d23729aef4c3cf6449c80ee
Sulimoff <ssu95a@gmail.com>
2026-07-07 13:38:29 +0300

    JInvTreeTableColumnEx_Boolean

--- column/JInvTreeTableColumnEx_Boolean.java
+++ column/JInvTreeTableColumnEx_Boolean.java
@@ -1,6 +1,11 @@
 package ru.inversion.fx.form.controls.treetableex.column;
 
+import ru.inversion.dataset.fx.ICellValueChangeListener;
+import ru.inversion.fx.app.Tags;
 import ru.inversion.fx.form.controls.treetableex.JInvTreeTableColumnEx;
+import ru.inversion.meta.IEntityProperty;
+
+import java.util.Objects;
 
 /** */
 public class JInvTreeTableColumnEx_Boolean<P> extends JInvTreeTableColumnEx<P, Boolean> {
@@ -21,5 +26,21 @@ public class JInvTreeTableColumnEx_Boolean<P> extends JInvTreeTableColumnEx<P, B
         setMinWidth ( FIXED_WIDTH );
         setPrefWidth( FIXED_WIDTH );
         setMaxWidth ( 40 );
+    }
+
+    @Override
+    public void bind( IEntityProperty<P, Boolean> ep, ICellValueChangeListener<P> cellValueChangeListener )
+    {
+        if( entityProperty != null )
+            throw new IllegalStateException( Tags.PRODUCT_LABEL + "Column '" + getFieldName() + "' already bind" );
+
+        final IEntityProperty<P, Boolean> property = Objects.requireNonNull( ep, "'entityProperty' is null" );
+
+        final Class<?> propertyType = property.getType();
+
+        if( propertyType != boolean.class && propertyType != Boolean.class )
+            throw new IllegalStateException( Tags.PRODUCT_LABEL + "Column '" + getFieldName() + "' is not boolean type" );
+
+        super.bind( property, cellValueChangeListener );
     }
 }
